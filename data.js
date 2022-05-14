b25265e506cf11c022cdcc6e3f16a1116d24d40b
Weijing Lin <weijingjaylin@gmail.com>
2022-05-14 03:13:44 -0700

    initial commit

---
+++ data.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    name: "泡泡弓箭",
+    price: "",
+    shipping: "",
+    name_en: "Bobble Archery",
+    categories: ["toy"],
+    media: {
+      images: ["./images/product/001/a.png", "./images/product/001/b.png"],
+      videos: ["./videos/products/001/a.mp3"],
+    },
+    properties: [
+      { color: "blue/pink" },
+      { package: "34*31*7cm" },
+      { weight: "1kg" },
+      { includes: "60 ml bobble bottle" },
+      { age: "7-14" },
+    ],
+    options: ["blue", "pink", "random"],
+  },
+  {
+    name: "充电线",
+    name_en: "Bobble Archery",
+    price: "",
+    shipping: "",
+    categories: ["toy"],
+    media: {
+      images: ["./images/product/001/a.png", "./images/product/001/b.png"],
+      videos: ["./videos/products/001/a.mp3"],
+    },
+    description: "尼龙编织线, pc 材质",
+    properties: [
+      { color: "blue/pink" },
+      { length: "1m/2m" },
+      { weight: "1kg" },
+      { includes: "60 ml bobble bottle" },
+    ],
+    options: ["1m", "2m"],
+  },
+];
